f92a11ade7f12801d496997d28e3801ed0c4a009
nhidayahj <hidayah.joha@gmail.com>
2021-01-28 09:55:12 +0000

    final q3 solution

--- hands-on/q3-combine-multiple-files/script.js
+++ hands-on/q3-combine-multiple-files/script.js
@@ -1,24 +1,15 @@
-let loadBtn = document.querySelector("#load-btn")
-let itemArr = [];
+let loadBtn = document.querySelector('#load-btn');
 
-async function loadData() {
-    let response1 = await axios.get("file1.txt");
-    let response2 = await axios.get("file2.txt");
+loadBtn.addEventListener('click', async function(){
+    let response1 = await axios.get('file1.txt');
+    let response2 = await axios.get('file2.txt');
 
-    let file1Content = response1.data;
-    let file2Content = response2.data;
+    // console.log(response1.data, response2.data);
+   
+    let newTextArea = document.querySelector("#content")
+    let contents = `${response1.data}\n${response2.data}
+    `
+    newTextArea.innerHTML = contents;
 
-    file1Content = file1Content.split("\n")
-    file2Content = file2Content.split("\n");
 
-    newArr = file1Content.concat(file2Content);
-}
-
-loadBtn.addEventListener('click', function(){
-    loadData();
-    
-    for (let i = 0; i<itemArr.length; i++) {
-        console.log(itemArr[i])
-    }
-
-})
+}); // end event listener
