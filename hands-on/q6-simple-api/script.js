3c878f084592196ef8de0c45914fadf88c7bac57
nhidayahj <hidayah.joha@gmail.com>
2021-01-31 17:35:29 +0000

    trouble loading url

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,2 +1,11 @@
 
 let url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+
+// create event listener 
+// let randomBtn = document.querySelector("#load-random");
+// randomBtn.addEventListener('click', async () => {
+//     // alert("connected!")
+//     // await for async to return the data
+//     let response = await axios.get(url);
+//     console.log(response.data)
+// })
